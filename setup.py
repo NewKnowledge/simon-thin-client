3f5f8454f4b32543aaf4f9a52ed45e05e383b1ec
David Sullivan <davesullivan41@gmail.com>
2017-07-27 16:57:32 +0000

    Add setup.py so the thin client can be installed on another machine

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='SimonThinClient',
+    version='1.0',
+    description='A thin client for interacting with Popily primitives',
+    py_modules=['client'])
