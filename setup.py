6fb1e39c4d99165b4cb1bfcef9224adb10488877
azunre <azunre@gmail.com>
2017-09-22 14:51:24 -0500

    renaming folders for consistency

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='SimonThinClient',
     version='1.0',
-    description='A thin client for interacting with Popily primitives',
-    py_modules=['simon-thin-client'])
+    description='A thin client for interacting with dockerized Simon primitive',
+    py_modules=['SimonThinClient'])
