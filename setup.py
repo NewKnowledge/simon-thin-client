cef30738f2d7a13b0b39f5dcca5f91df20953f30
azunre <azunre@gmail.com>
2017-09-26 16:18:02 -0500

    minor fixes to flask interface

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='SimonThinClient',
     version='1.0',
     description='A thin client for interacting with dockerized simon primitive',
     packages=['SimonThinClient'],
-    install_requires=["numpy","pandas","pickle","requests","ast","abc","json","typing"],
+#   install_requires=["numpy","pandas","pickle","requests","ast","abc","json","typing"],
 #    entry_points = {
 #        'd3m.primitives': [
 #            'distil.simon = SimonThinClient.client:SimonThinClient'
