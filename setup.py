b93d835b22632217fcec5364cc96a93f69ab0219
azunre <azunre@gmail.com>
2017-09-26 17:30:07 -0500

    case sensitivity in folder name...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='SimonThinClient',
     version='1.0',
     description='A thin client for interacting with dockerized simon primitive',
-    packages=['SsimonThinClient'],
+    packages=['SimonThinClient'],
 #   install_requires=["numpy","pandas","pickle","requests","ast","abc","json","typing"],
 #    entry_points = {
 #        'd3m.primitives': [
